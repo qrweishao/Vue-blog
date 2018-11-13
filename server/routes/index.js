c2ab6a5f389134ed26eebe3c53a985dfd7eefed4
qrweishao <2529732778@qq.com>
2018-11-13 17:51:05 +0800

    搭建登录模块。。。。

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 import Router from 'koa-router'
 import { base_API } from '../config'
+// 引入登录注册的控制器
+import UserController from '../Controller/UserController'
 const router = new Router()
 router.get('/',async ctx=>{
     ctx.body = '欢迎使用vue-blog接口测试服务器'
@@ -9,8 +11,7 @@ router.get('/',async ctx=>{
 // 前端所有的请求过来的时候都是http://localhost:3000/api/...
 router.prefix(`${base_API}`)
 
-router.get('/index',async ctx=>{
+// 登录
+router.post('/login',UserController.login)
 
-    ctx.body='我是首页的接口'
-})
 export default router
