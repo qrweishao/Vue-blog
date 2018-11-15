a3a0590531a40720a0eb0345fd4fe11f14884abd
qrweishao <2529732778@qq.com>
2018-11-15 17:54:15 +0800

    tags出不来。。。

--- admin/src/components/common/ArticleList.vue
+++ admin/src/components/common/ArticleList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <ul class="list">
-            <li class="article" :class="{active: activeIndex === index, published: isPublished === 1}" v-for="{title, createTime, isPublished, isChosen},index in articleList">
+            <li class="article" :class="{active: activeIndex === index, published: isPublished === 1}" v-for="{title, createTime, isPublished, isChosen},index in articleList" @click="select(index)">
                 <header>{{title}}</header>
                 <p>{{createTime}}</p>
             </li>
@@ -25,7 +25,6 @@ export default {
     // 把全局的vuex里面的state和mutations放到计算属性中
     computed:{
         ...mapState(['id','title', 'tags', 'content', 'isPublished', 'toggleDelete']),
-        ...mapMutations(['SET_CURRENT_ARTICLE'])
     },
     // 当该组件创建的时候自动执行里面的请求
     created() {
@@ -38,7 +37,11 @@ export default {
                 article.isChosen = true
             }
             this.articleList.push(...res)
-            // console.log(this.articleList)
+            // 如果查询出文章 则将第一篇文章作为正在编辑的文章
+            if (this.articleList.length !== 0) {
+             this.SET_CURRENT_ARTICLE(this.articleList[0])
+             this.activeIndex = 0;   
+            }
         }).catch(err=>{
             console.log(err)
         })
@@ -53,10 +56,19 @@ export default {
                 article.createTime = moment(article.createTime).format('YYYY年-MM月-DD日 HH:mm:ss')
                 article.isChosen = true
                 this.articleList.unshift(article)
+                // 如果发布了新文章的话 当前被选中的文章下标自动加一
+                this.activeIndex ++
+                
             }).catch(err=>{
                 console.log(err)
             })
-        }
+        },
+        select(index){
+            this.activeIndex = index
+            // 当你在选择文章的时候 当前被选中的文章扔到全局状态管理里面
+            this.SET_CURRENT_ARTICLE(this.articleList[index])
+        },
+         ...mapMutations(['SET_CURRENT_ARTICLE'])
     }
 }
 </script>
